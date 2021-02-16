ea8134ecc54582c53667d0291569d6aad5fd0c63
VCP <vcp@ap.xom.com>
2021-02-16 18:30:10 +0700

    [Roj] create customer table and insert default data

---
+++ src/main/resources/db/migration/V2021_02_15_11_22__create-table_customer.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset database-migration:1
+
+create TABLE customer
+(
+    id         int IDENTITY(1,1) primary key,
+    first_name varchar(50),
+    last_name  varchar(50),
+    age        int
+);
---
+++ src/main/resources/db/migration/V2021_02_16_18_18__insert_default_data_table_customer.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset database-migration:2
+
+INSERT INTO customer (first_name, last_name, age) values ('John', 'Doe', 31);
+INSERT INTO customer (first_name, last_name, age) values ('Karl', 'Jablonski', 45);
+INSERT INTO customer (first_name, last_name, age) values ('Matti', 'Karttunen', 31);
+INSERT INTO customer (first_name, last_name, age) values ('Wolski', 'Zbyszek', 18);
